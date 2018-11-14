f8972effcc052238d27e29bddd98a37c5c885ced
ZohrabMusayev <zmusayev2019@ada.edu.az>
2018-11-14 17:45:54 +0400

    Tank Creation and movement

---
+++ Class-Game-Project/Assets/Tank/MoveTank.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class MoveTank : MonoBehaviour
+{
+    public float speedMove = 1.0F;
+    public float speedRotate = 0.5F;
+
+    void Update()
+    {
+        CharacterController cntrlr = GetComponent<CharacterController>();
+
+        // Rotate around y - axis
+        transform.Rotate(0, Input.GetAxis("Horizontal") * speedRotate, 0);
+
+        // Move forward / backward
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speedMove * Input.GetAxis("Vertical");
+        cntrlr.SimpleMove(forward * curSpeed);
+    }
+}
