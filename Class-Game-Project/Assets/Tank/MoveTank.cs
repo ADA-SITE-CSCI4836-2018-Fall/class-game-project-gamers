82070640828e50bdcd5079808f3038e10b7c7ea6
ZohrabMusayev <zmusayev2019@ada.edu.az>
2018-11-19 16:18:25 +0400

    PuttingBomb

--- Class-Game-Project/Assets/Tank/MoveTank.cs
+++ Class-Game-Project/Assets/Tank/MoveTank.cs
@@ -6,7 +6,7 @@ public class MoveTank : MonoBehaviour
 {
     public float speedMove = 1.0F;
     public float speedRotate = 0.5F;
-
+    public GameObject Bomb;
     void Update()
     {
         CharacterController cntrlr = GetComponent<CharacterController>();
@@ -18,5 +18,11 @@ public class MoveTank : MonoBehaviour
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         float curSpeed = speedMove * Input.GetAxis("Vertical");
         cntrlr.SimpleMove(forward * curSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            {
+            var a = new Vector3(transform.position.x, transform.position.y + 0.1f, transform.position.z);
+         Instantiate(Bomb, a, Quaternion.identity);
+        }
     }
 }
